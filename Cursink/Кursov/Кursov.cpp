3bb0a0b4a6d175ea6c7a04ba5ece64c2ea04e17f
Degroshka <dmitr03@ngs.ru>
2023-06-06 13:29:27 +0700

    3rd

--- Cursink/Кursov/Кursov.cpp
+++ Cursink/Кursov/Кursov.cpp
@@ -11,10 +11,14 @@ struct Sdol {
     char elem;
     Sdol* next, * prev;
 };
+struct Prov {
+    char elem;
+    Prov* next;
+};
 int provfin(Fdol* f,Fdol *ff, Sdol* s, Sdol* ss) {
     while (f->next != nullptr) {
         while (s->next != nullptr) {
-            cout << f->elem << " and " << s->elem << endl;
+            cout << f->elem << " && " << s->elem << endl;
             if (f->elem == s->elem) {
                 return 0;}
             s = s->next;}
@@ -30,8 +34,11 @@ int proverF(Fdol *pf, Sdol *ps,Sdol* ss, Fdol* ff,  char fch, char sch) {   ////
             ss = ps;
             while (ss->elem != NULL) {
                 cout << fch << " и " << sch << " И ТАБЛ 1.5.... " << ff->elem << " " << ss->elem << endl;
-                if (sch == ss->elem) {
+                if (sch == ss->elem and fch==ff->elem) {
                     cout << fch << sch << " Не берем"<<endl;
+                    return 2;
+                }
+                else if(sch==ss->elem){
                     return 4;
                 }
                 ss = ss->next;
@@ -43,16 +50,16 @@ int proverF(Fdol *pf, Sdol *ps,Sdol* ss, Fdol* ff,  char fch, char sch) {   ////
             ss = ps;
             while (ss->elem != NULL) {
                 cout << fch << " и " << sch << " И ТАБЛ 2.5... " << ff->elem << " " << ss->elem << endl;
-                if (fch == ss->elem and sch == ff->elem) {
+                if (fch == ss->elem or sch==ss->elem) {
                     return 2;
                 }
-                else if (sch == ss->elem or fch==ss->elem) {
-                    return 3;
+                else if (sch == ss->elem) {
+                    return 4;
                 }
                
                 ss = ss->next;
             }
-                return 2;
+                return 1;
             }
             if (ff->elem != NULL)ff = ff->next;
             if (ss->elem != NULL)ss = ss->next;
@@ -60,9 +67,61 @@ int proverF(Fdol *pf, Sdol *ps,Sdol* ss, Fdol* ff,  char fch, char sch) {   ////
         return 0;
     }
 
+int provnaSv() {
+    ifstream fin("sym.txt");
+    Prov* pr,*pE,*pF,*an;
+    char som;
+    if (fin.is_open()) {
+    pr = pE = pF = an = new Prov;
+    do {
+        fin >> som; 
+            pr->next = new Prov;
+            pr->next->elem = NULL; 
+            if (fin.eof()) {
+                break;
+            } pr->elem = som;
+            pr = pr->next;
+        
+    } while (!fin.eof());
+    pr->next = nullptr;
+    fin.close();
+}
 
+    else {
+        cout << "Ошибка открытия файла." << endl;
+        return 3;
+    }
+    while (an!= nullptr) {
+        an = an->next;
+    }
+    pr = pE;
+    Prov* current = pE;
+    while (pE->elem != NULL) {
 
-    
+        cout << pE->elem<<endl;
+
+        while (pr->elem != NULL) {
+            cout << pr->elem<<" and "<<pr->next->elem << endl;
+            if ((pr->elem == pE->elem || pr->elem == pE->next->elem || pr->next->elem == pE->elem || pr->next->elem == pE->next->elem) && (pr != pE && pr->next != pE->next && pr!=pE->next && pr->next!=pE))
+            {
+                cout << "одинаково" << endl;
+                break;
+            }             
+            pr = pr->next->next;
+            if (pr->elem == NULL) {
+                return 0;
+            }
+        }
+        pr = pF;    
+        pE = pE->next->next;
+    } 
+    while (current != nullptr) {
+        Prov* next = current->next;
+        delete current;
+        current = next;
+    }
+    return 1;
+}
     
     
     
@@ -85,6 +144,10 @@ void print(Fdol* f, Sdol* s) {
 
 int main() {
     setlocale(LC_ALL, "Russian");
+    if (provnaSv() == 0) {
+        cout << "Граф не связан";
+        return 0;
+    }
     ifstream fin("sym.txt"); // открываем файл для чтения
     if (!fin.is_open()) { // если файл не открыт
         cout << "Ошибка открытия файла!" << endl;
@@ -111,8 +174,7 @@ int main() {
                 s->elem = sch;
             }
             else if (proverF(pf, ps,ss, ff, fch, sch) == 3) {
-                f->elem = sch;
-                
+                f->elem = sch;               
             }
             else if(proverF(pf,ps,ss, ff, fch, sch) == 0)
             {
